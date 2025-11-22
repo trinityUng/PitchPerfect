4d8bdd636a2babc1b56284d6db274ebbb6af8476
Trinity Ung <tung3@uwo.ca>
2025-11-22 09:41:15 -0500

    login page yayyyyy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import './index.css'
+import Profile from "./pages/Profile.jsx";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} /> 
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
