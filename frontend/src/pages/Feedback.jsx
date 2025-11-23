bf10bb5b01215bd72cd389548df2986f3eb05811
Trinity Ung <tung3@uwo.ca>
2025-11-23 05:34:44 -0500

    fixed feedback page

--- frontend/src/pages/Feedback.jsx
+++ frontend/src/pages/Feedback.jsx
@@ -86,64 +86,73 @@ export default function Feedback() {
           }}
         >
           {/* BOTTOM ROW */}
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "flex-end",
-              paddingLeft: "40px",
-              paddingRight: "40px",
-
-              paddingBottom: "20px",
-            }}
-          >
-            <img
-              src="/images/presentAgain.png"
-              alt="Left button"
-              className="button-image"
+            <div
               style={{
-                width: "105px",
-                height: "90px",
-                cursor: "pointer",
-                marginLeft: "25px",
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                paddingBottom: "0px",
               }}
-              onClick={() => navigate("/present")}
-            />
+            >
 
-            <img
-              src="/images/binoGoose.png"
-              alt="feedback icon"
+            <h1
               style={{
-                width: "700px",
-                marginBottom: "-20px",
+                textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
+                fontSize: "3rem",
+                marginBottom: "1rem",
+                marginTop: "0",
+                textAlign: "center",
+                color: "#1E406E",
               }}
-            />
+            >
+              Duck, duck… boost! Click the icons below to receive your feedback!
+            </h1>
 
-            <img
-              src="/images/bluepdf.png"
-              alt="download PDF"
-              style={{
-                width: "50px",
-                cursor: "pointer",
-                marginRight: "30px",
-              }}
-              onClick={downloadPDF}
-            />
+              {/* Button Row Centered */}
+              <div
+                style={{
+                  display: "flex",
+                  gap: "2rem",
+                  marginTop: "1rem",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src="/images/presentAgain.png"
+                  alt="present again"
+                  className="button-image"
+                  style={{ width: "105px", height: "90px", cursor: "pointer" }}
+                  onClick={() => navigate("/present")}
+                />
 
-            <img
-            src="/images/export.png"
-            alt="Download Latest Video"
-            onClick={downloadLatestVideo}
-            style={{
-                width: "70px",
-                cursor: "pointer",
-                marginRight: "40px",
-            }}
+                <img
+                  src="/images/bluepdf.png"
+                  alt="download PDF"
+                  className="button-image"
+                  onClick={downloadPDF}
+                  style={{ width: "60px", cursor: "pointer" }}
+                />
 
-            />
-          </div>
-        </div>
+                <img
+                  src="/images/export.png"
+                  alt="download video"
+                  className="button-image"
+                  onClick={downloadLatestVideo}
+                  style={{ width: "70px", cursor: "pointer" }}
+                />
+              </div>
+                <img
+                  src="/images/binGoose.png"
+                  alt="feedback icon"
+                  style={{
+                    width: "300px",
+                    marginTop: "20px",   
+                  }}
+                />
+            </div>
+            </div>
       </Layout>
     </div>
   );
