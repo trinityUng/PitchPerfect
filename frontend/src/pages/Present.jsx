fb3dc16bf338a7352e45d28228d2a55a1dd8d8d2
amelpiat <132037532+amelpiat@users.noreply.github.com>
2025-11-23 00:21:27 -0500

    fixing camera functionality

--- frontend/src/pages/Present.jsx
+++ frontend/src/pages/Present.jsx
@@ -58,8 +58,10 @@ export default function Present() {
   };
 
   /* START RECORDING */
-  const startRecording = () => {
-    if (!streamRef.current) return;
+  const startRecording = async () => {
+    if (!streamRef.current){
+      await startCamera();
+    };
 
     enterFullscreen();
     recordingFlagRef.current = true;
@@ -190,6 +192,11 @@ export default function Present() {
     loopAudioRecorderRef.current?.stop();
     loopVideoRecorderRef.current?.stop();
 
+    if(streamRef.current){
+      streamRef.current.getTracks().forEach((t) => t.stop());
+      streamRef.current = null;
+    }
+
     // wait a bit for uploadFullVideo() to run
     setTimeout(() => {
       navigate("/feedback");
@@ -315,10 +322,6 @@ export default function Present() {
       console.error("Failed to upload full video:", err);
     }
   };
-
-  useEffect(() => {
-    startCamera();
-  }, []);
 
   return (
     // <Layout>
